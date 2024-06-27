4ab81b418a2edb4dce083f4b196bb2b67a18ebff
Abigail Wang <abbiesiru@abbies-air-2022.lan>
2024-06-27 10:04:21 -0700

    6/27/24

--- scripts/filter_samples.R
+++ scripts/filter_samples.R
@@ -1,16 +1,19 @@
 # load the expression data matrix first into a variable called "X01_expressMatrix_geneLevel_RSEM_TPM" or something simpler
+readr::read_delim
 
+express_matrix <- read.csv("dataset/01_expressMatrix.geneLevel_RSEM_TPM.csv", check.names=FALSE);
+meta_data <- read_xlsx("dataset/manifest_source-data_RNA-Seq_MB.xlsx", col_names = TRUE);
 
-sample_name2 = as.list(manifest_source_data_RNA_Seq_MB$sample_name)
+sample_name2 = as.list(meta_data$sample_name)
 
 '%!in%' <- function(x,y)!('%in%'(x,y))
-for (i in colnames(X01_expressMatrix_geneLevel_RSEM_TPM)){
+for (i in colnames(express_matrix)){
   if(i %!in% sample_name2 && i!= "gene_id" && i!= "gene_name" && i!= "gene_type") {
-    X01_expressMatrix_geneLevel_RSEM_TPM[[i]] <- NULL
+    express_matrix[[i]] <- NULL
   }
 }
-colnames(X01_expressMatrix_geneLevel_RSEM_TPM)
+colnames(express_matrix)
 
-# Save the filered data into a new file
+# Save the filered data into a new file that can be loaded back into R as an ExpressionSet class object
 
                      
