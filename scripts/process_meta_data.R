72eb0c603a2fa1d2cb006a69a382af72ef8b5990
Abigail Wang <abbiesiru@abbies-air-2022.lan>
2024-06-27 12:01:30 -0700

    07/27/24 12:01

--- scripts/process_meta_data.R
+++ scripts/process_meta_data.R
@@ -3,20 +3,23 @@ library(dplyr)
 install.packages("stringr")
 library(stringr)
 
-manifest_source_data_RNA_Seq <- data.read("");
+library(readxl)
 
-manifest_source_data_RNA_Seq_MB <- dplyr::filter(manifest_source_data_RNA_Seq, disease_type == "Medulloblastoma")
-View(manifest_source_data_RNA-Seq_MB)
+# Read meta data file into R as a dataframe
+meta_data <- read_xls("manifest_source-data_RNA-Seq.xls", col_names=TRUE);
+
+meta_data_MB <- dplyr::filter(meta_data, disease_type == "Medulloblastoma")
+View(meta_data_MB)
 
 
-manifest_source_data_RNA_Seq_MB$sample_name <- ifelse(grepl(".local.transcript.bam", manifest_source_data_RNA_Seq_MB$name),str_sub(manifest_source_data_RNA_Seq_MB$name, 13, -22), str_sub(manifest_source_data_RNA_Seq_MB$name, 13, -5))
-manifest_source_data_RNA_Seq_MB$sample_name <- ifelse(grepl(".fq.gz", manifest_source_data_RNA_Seq_MB$name),str_sub(manifest_source_data_RNA_Seq_MB$name, 13, -7), manifest_source_data_RNA_Seq_MB$sample_name)
-View(manifest_source_data_RNA_Seq_MB)
+meta_data_MB$sample_name <- ifelse(grepl(".local.transcript.bam", meta_data_MB$name),str_sub(meta_data_MB$name, 13, -22), str_sub(meta_data_MB$name, 13, -5))
+meta_data_MB$sample_name <- ifelse(grepl(".fq.gz", meta_data_MB$name),str_sub(meta_data_MB$name, 13, -7), meta_data_MB$sample_name)
+View(meta_data_MB)
 
-manifest_source_data_RNA_Seq_MB <- manifest_source_data_RNA_Seq_MB %>%
+meta_data_MB <- meta_data_MB %>%
   dplyr::relocate(sample_name,.after = name)
-View(manifest_source_data_RNA_Seq_MB)
+View(meta_data_MB)
 
 install.packages("openxlsx")
 library(openxlsx)
-write.xlsx(manifest_source_data_RNA_Seq_MB, 'manifest_source-data_RNA-Seq_MB.xlsx')
+write.xlsx(meta_data_MB, 'manifest_source-data_RNA-Seq_MB.xlsx')
