459de448c30275292f58579ebdadcd6b26954ce1
Abbiesiru <abbiesiru@gmail.com>
2025-02-15 19:57:08 -0800

    Add files via upload

--- scripts/filter_samples.R
+++ scripts/filter_samples.R
@@ -1,48 +1,37 @@
-setwd("/Users/abbiesiru/Desktop/research/medulloblastoma/dataset")
-library(readxl);
 library(dplyr)
 
+setwd("/Users/abbiesiru/Desktop/research/medulloblastoma")
+
 # read expression matrix file into R as a dataframe
-tmp <- read.delim("01_expressMatrix.geneLevel_RSEM_TPM.txt", check.names=FALSE);
+tmp <- read.delim("dataset/01_expressMatrix.geneLevel_RSEM_TPM.txt", check.names=FALSE);
 express_matrix <- dplyr::filter(tmp, gene_type=="protein_coding")
 
-meta_data <- read_xlsx("manifest_source-data_RNA-Seq_MB.xlsx", col_names = TRUE);
-
-sample_name2 = as.list(meta_data$sample_name)
+meta_data <- read.csv("dataset/manifest_source-data_RNA-Seq_MB.csv");
 
 '%!in%' <- function(x,y)!('%in%'(x,y))
-meta_data_idx <- list()
-# initiate a empty list that store the index of found sample in the meta data
-# idx <- [];
-for (i in colnames(express_matrix)){
-  if(i %!in% sample_name2 && i!= "gene_id" && i!= "gene_name" && i!= "gene_type") {
-    express_matrix[[i]] <- NULL
-  } else if((i == "gene_id" | i== "gene_name" | i== "gene_type"))
-    express_matrix[[i]]
+
+# initiate a empty vector that store the index of found sample in the meta data
+meta_data_idx <- c();
+rm_col_idx <- c();
+
+# for (i in colnames(express_matrix)){
+for (i in 4:ncol(express_matrix)) {
+  if (colnames(express_matrix)[i] %!in% meta_data$sample_name) {
+    rm_col_idx <- c(rm_col_idx, i);
+  } 
   else {
-    idx <- match(i, sample_name2)
-    meta_data_idx <- c(meta_data_idx, idx )
     # return the index of the found sample in the meta data
-    # append the index to the end of the list created above
+    idx <- match(colnames(express_matrix)[i], meta_data$sample_name)
+    # append the index to the end of the vector created above
+    meta_data_idx <- c(meta_data_idx, idx )
+    
   }
 }
 
-meta_data_subset <- data.frame(matrix(ncol = 24, nrow = 0))
-colnames(meta_data_subset) <- c("id", "name", "sample_name", "size", "project", "visible", "Kids First Participant ID", "ethnicity", "gender", "race", "disease_type", "sample_id", "Tumor Descriptor", "sample_type", "paired_end", "platform", "Kids First Biospecimen ID", "primary_site", "age_at_diagnosis", "aliquot_id", "library_id", "reference_genome", "case_id", "experimental_strategy")
-tmp <- data.frame(matrix(ncol = 24, nrow = 0))
-colnames(tmp) <- c("id", "name", "sample_name", "size", "project", "visible", "Kids First Participant ID", "ethnicity", "gender", "race", "disease_type", "sample_id", "Tumor Descriptor", "sample_type", "paired_end", "platform", "Kids First Biospecimen ID", "primary_site", "age_at_diagnosis", "aliquot_id", "library_id", "reference_genome", "case_id", "experimental_strategy")
+express_matrix[,rm_col_idx] <- NULL;
+meta_data_subset <- meta_data[meta_data_idx,];
 
-for (i in meta_data_idx){
- tmp <-  filter(meta_data, sample_name %in% sample_name2[i])
-  meta_data_subset <- rbind(meta_data_subset, tmp)
-}
-
-
-
-write.csv(express_matrix, "01_expressMatrix.geneLevel_RSEM_TPM_filtered.csv",row.names=FALSE)
-write.csv(meta_data_subset, "manifest_source-data_RNA-Seq_MB_subset.csv",row.names=FALSE)
-
-# Output the filtered meta data, where the row ordering is the same as the column ordering of the expression data
-
+write.csv(express_matrix, "dataset/01_expressMatrix.geneLevel_RSEM_TPM_filtered.csv",row.names=FALSE)
+write.csv(meta_data_subset, "dataset/manifest_source-data_RNA-Seq_MB_subset.csv",row.names=FALSE)
 
                      
--- scripts/generate_eset.R
+++ scripts/generate_eset.R
@@ -1,6 +1,5 @@
 library(NetBID2);
 
-
 setwd("/Users/abbiesiru/Desktop/research/medulloblastoma/")
 
 tmp <- read.csv("dataset/01_expressMatrix.geneLevel_RSEM_TPM_filtered.csv", check.names=FALSE, row.names="gene_id");
@@ -9,20 +8,430 @@ feature_info <- tmp[,1:2];
 
 meta_data <- read.csv("dataset/manifest_source-data_RNA-Seq_MB_subset.csv", row.names = "sample_name")
 
+# These are the meta data columns to use in the QC
+idx <- c("ethnicity","gender","race","Tumor.Descriptor","age_at_diagnosis"); # "primary_site"
+meta_data <- meta_data[,idx];
+
+#log2-transformation
 net_eset <- generate.eset(
-  exp_mat = exp_mat,
+  exp_mat = log2(exp_mat+1),
   phenotype_info = meta_data,
   feature_info = feature_info,
 )
 
+# exprs(net_eset)<-log2(exprs(net_eset)+1) 
+
 project_main_dir <- './' # user defined main directory for the project, one main directory could have multiple project folders, distinguished by project name.
 project_name <- 'project'
 
+if (exists("network.par")) {
+  rm(network.par)
+}
 network.par  <- NetBID.network.dir.create(project_main_dir=project_main_dir,project_name=project_name)
-
 network.par$net.eset <- net_eset
 
 draw.eset.QC(network.par$net.eset,outdir=network.par$out.dir.QC,intgroup=NULL,do.logtransform=FALSE,prefix='beforeQC_',emb_plot_type='2D.interactive')
+
+###  The following QC steps are optional
+## Firstly, the handling of missing data.
+# Get the expression matrix from ExpressionSet object
+mat <- exprs(network.par$net.eset)
+# Count and show number of NAs across samples and genes
+sample_na_count <- apply(mat,1,function(x){length(which(is.na(x)==TRUE))})
+print(table(sample_na_count))
+gene_na_count <- apply(mat,2,function(x){length(which(is.na(x)==TRUE))})
+print(table(gene_na_count))
+# Perform imputation
+if(sum(sample_na_count)+sum(gene_na_count)>0) mat <- impute.knn(mat)$data
+
+## Secondly, the log2 transformation.
+# med_val <- median(apply(mat,2,median)); print(med_val)
+# if(med_val>16){mat <- log2(mat)}
+
+## Thirdly, the quantile normalization across samples.
+# Perform limma quantile normalization
+mat_norm <- normalizeQuantiles(mat)
+
+## Fourthly, filter out genes with very low expression values (bottom 5%) in most samples (more than 90%).
+# Filter out low-expression genes
+choose1 <- apply(mat_norm<=quantile(mat_norm, probs = 0.05), 1, sum)<=(ncol(mat_norm)*0.80)
+print(table(choose1))
+mat_norm_filt <- mat_norm[choose1,]
+
+# Update eset with normalized expression matrixs
+net_eset <- generate.eset(exp_mat=mat_norm_filt, phenotype_info=pData(network.par$net.eset)[colnames(mat_norm_filt),],
+                          feature_info=fData(network.par$net.eset)[rownames(mat_norm_filt),])
+# Update network.par with new eset
+network.par$net.eset <- net_eset
+
+# QC for the normalized eset
+draw.eset.QC(network.par$net.eset,outdir=network.par$out.dir.QC,intgroup=NULL,do.logtransform=FALSE,prefix='afterQC_',emb_plot_type='2D.interactive')
+
+saveRDS(net_eset, 'dataset/dataset1.rds')
+
+
+pdf(file="project/DATA/pca_basic_ARI.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "pca",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "basic",
+  choose_k_strategy = "ARI",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+pdf(file="project/DATA/pca_basic_NMI.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "pca",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "basic",
+  choose_k_strategy = "NMI",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+pdf(file="project/DATA/pca_basic_Jaccard.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "pca",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "basic",
+  choose_k_strategy = "Jaccard",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+pdf(file="project/DATA/pca_consensus_ARI.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "pca",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "consensus",
+  choose_k_strategy = "ARI",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+pdf(file="project/DATA/pca_consensus_NMI.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "pca",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "consensus",
+  choose_k_strategy = "NMI",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+pdf(file="project/DATA/pca_consensus_Jaccard.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "pca",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "consensus",
+  choose_k_strategy = "Jaccard",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+pdf(file="project/DATA/mds_basic_ARI.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "mds",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "basic",
+  choose_k_strategy = "ARI",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+pdf(file="project/DATA/mds_basic_NMI.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "mds",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "basic",
+  choose_k_strategy = "NMI",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+pdf(file="project/DATA/mds_basic_Jaccard.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "mds",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "basic",
+  choose_k_strategy = "Jaccard",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+pdf(file="project/DATA/mds_consensus_ARI.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "mds",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "consensus",
+  choose_k_strategy = "ARI",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+pdf(file="project/DATA/mds_consensus_NMI.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "mds",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "consensus",
+  choose_k_strategy = "NMI",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+pdf(file="project/DATA/mds_consensus_Jaccard.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "mds",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "consensus",
+  choose_k_strategy = "Jaccard",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+pdf(file="project/DATA/umap_basic_ARI.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "umap",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "basic",
+  choose_k_strategy = "ARI",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+pdf(file="project/DATA/umap_basic_NMI.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "umap",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "basic",
+  choose_k_strategy = "NMI",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+pdf(file="project/DATA/umap_basic_Jaccard.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "umap",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "basic",
+  choose_k_strategy = "Jaccard",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+pdf(file="project/DATA/umap_consensus_ARI.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "umap",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "consensus",
+  choose_k_strategy = "ARI",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+pdf(file="project/DATA/umap_consensus_NMI.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "umap",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "consensus",
+  choose_k_strategy = "NMI",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+pdf(file="project/DATA/umap_consensus_Jaccard.pdf",width=16,height=8)
+draw.emb.kmeans(
+  mat = exprs(net_eset),
+  embedding_method = "umap",
+  all_k = NULL,
+  obs_label = get_obs_label(meta_data, use_col="ethnicity"),
+  legend_pos = "topleft",
+  legend_cex = 0.8,
+  plot_type = "2D.ellipse",
+  point_cex = 1,
+  kmeans_strategy = "consensus",
+  choose_k_strategy = "Jaccard",
+  return_type = "optimal",
+  main = "",
+  verbose = TRUE,
+  use_color = NULL,
+  pre_define = NULL
+)
+dev.off()
+
+
+
 # If use the same expression dataset as in the network construction, just reload it directly
 load(sprintf('%s/DATA/network.par.Step.exp-QC.RData',network.dir)) # RData saved after QC in the network construction step
 analysis.par$cal.eset <- network.par$net.eset
@@ -39,3 +448,4 @@ analysis.par$sig.network <- get.SJAracne.network(network_file=analysis.par$sig.n
 # Creat QC report for the network
 draw.network.QC(analysis.par$tf.network$igraph_obj,outdir=analysis.par$out.dir.QC,prefix='TF_net_',html_info_limit=FALSE)
 draw.network.QC(analysis.par$sig.network$igraph_obj,outdir=analysis.par$out.dir.QC,prefix='SIG_net_',html_info_limit=TRUE)
+
